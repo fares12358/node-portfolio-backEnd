ec461b10dbca5d17b4d623ed403f6bd452de302a
fares mohamed <fm883254@gmail.com>
2025-04-21 16:01:09 +0200

    fix

--- server.js
+++ server.js
@@ -13,20 +13,23 @@ app.use(express.json()); // To parse JSON
 app.use(visitorRoutes); // Use our tracking route
 
 mongoose.connect(process.env.MONGO_URI, {})
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.error(err));
 
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.error(err));
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "hello portfolio back end" })
+})
 
 app.get('/visitor-count', async (req, res) => {
-    try {
-      const visitorDoc = await Visitor.findOne();
-      const totalVisits = visitorDoc ? visitorDoc.visits.length : 0;
-  
-      res.status(200).json({ total: totalVisits });
-    } catch (err) {
-      console.error('Error getting visitor count:', err);
-      res.status(500).json({ error: 'Failed to retrieve visitor count' });
-    }
-  });
+  try {
+    const visitorDoc = await Visitor.findOne();
+    const totalVisits = visitorDoc ? visitorDoc.visits.length : 0;
+
+    res.status(200).json({ total: totalVisits });
+  } catch (err) {
+    console.error('Error getting visitor count:', err);
+    res.status(500).json({ error: 'Failed to retrieve visitor count' });
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
